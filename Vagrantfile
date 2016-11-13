426df294d299cd8a43c634e3bd9eb6cb522073c3
Proustibat <jennifer.proust@gmail.com>
2016-11-14 00:08:21 +0100

    custom vhosts in a config file

--- Vagrantfile
+++ Vagrantfile
@@ -1,14 +1,27 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 
+DOMAIN_SCOTCHBOX="scotchbox.local"
+DOMAIN_SAMPLE="sample.local"
+DOMAIN_DEFAULT="projects.local"
+VAGRANTDIR="scotch-box-proustibat"
+vhostFile="vhosts.conf"
+
 Vagrant.configure("2") do |config|
 
     config.vm.box = "scotch/box"
     config.vm.network "private_network", ip: "192.168.33.10"
     config.vm.hostname = "scotchbox"
-    #config.vm.synced_folder "/workspace/projects", "/var/www", :mount_options => ["dmode=777", "fmode=666"]
     config.vm.synced_folder "/workspace", "/var/www", :nfs => { :mount_options => ["dmode=777","fmode=666"] }
 
-    config.vm.provision "shell", path: "provision-script.sh"
+    config.vm.provision "file",
+        source: vhostFile,
+        destination: vhostFile,
+        preserve_order: true
+
+    config.vm.provision "shell",
+        path: "provision-script.sh",
+        preserve_order: true,
+        args: [ DOMAIN_SCOTCHBOX, DOMAIN_SAMPLE, DOMAIN_DEFAULT, VAGRANTDIR, vhostFile ]
 
 end
--- provision-script.sh
+++ provision-script.sh
@@ -1,58 +1,61 @@
 #!/usr/bin/env bash
 
+if [[ -z $1 ]]; then DOMAIN_SCOTCHBOX="scotchbox.local";  else DOMAIN_SCOTCHBOX=$1; fi
+if [[ -z $2 ]]; then DOMAIN_SAMPLE="sample.local";        else DOMAIN_SAMPLE=$2; fi
+if [[ -z $3 ]]; then DOMAIN_DEFAULT="projects.local";     else DOMAIN_DEFAULT=$3; fi
+if [[ -z $4 ]]; then VAGRANTDIR="scotch-box-proustibat";  else VAGRANTDIR=$4; fi
+if [[ -z $5 ]]; then vhostFile="vhosts.conf";             else vhostFile=$5; fi
+
+
+# CREATE SAMPLE FILE
+   echo "Creating sample file config for vhosts..."
+   sudo cp /etc/apache2/sites-available/$DOMAIN_SCOTCHBOX.conf /etc/apache2/sites-available/$DOMAIN_SAMPLE.conf
+   sudo sed -i s,$DOMAIN_SCOTCHBOX,$DOMAIN_SAMPLE,g /etc/apache2/sites-available/$DOMAIN_SAMPLE.conf
+   sudo sed -i s,/var/www/public,/var/www/$DOMAIN_SAMPLE/public,g /etc/apache2/sites-available/$DOMAIN_SAMPLE.conf
+   sudo sed -i s,/var/www/$VAGRANTDIR/public,/var/www/$DOMAIN_SAMPLE/public,g /etc/apache2/sites-available/$DOMAIN_SAMPLE.conf
+
 # DEFAULT HOST
-    sudo sed -i s,/var/www/public,/var/www/projects,g /etc/apache2/sites-available/000-default.conf
-    sudo sed -i s,/var/www/public,/var/www/projects,g /etc/apache2/sites-available/scotchbox.local.conf
-    sudo service apache2 restart
+    echo "Setting default hosts..."
+    sudo cp /etc/apache2/sites-available/$DOMAIN_SAMPLE.conf /etc/apache2/sites-available/000-default.conf
+    sudo sed -i s,$DOMAIN_SAMPLE,$DOMAIN_DEFAULT,g /etc/apache2/sites-available/000-default.conf
+    sudo sed -i s,/var/www/$DOMAIN_DEFAULT/public,/var/www/projects,g /etc/apache2/sites-available/000-default.conf
 
+# SCOTCHBOX
+    echo "Updating vhost config for $DOMAIN_SCOTCHBOX..."
+    sudo sed -i s,/var/www/public,/var/www/$VAGRANTDIR/public,g /etc/apache2/sites-available/$DOMAIN_SCOTCHBOX.conf
+    echo "Enabling $DOMAIN_SCOTCHBOX..."
+    sudo a2ensite $DOMAIN_SCOTCHBOX.conf
 
-# BOWBOOSH
-    DOMAIN=bowboosh.local
-    echo "Creating vhost config for $DOMAIN..."
-    sudo cp /etc/apache2/sites-available/scotchbox.local.conf /etc/apache2/sites-available/$DOMAIN.conf
+# CUSTOM VHOSTS
+while IFS='' read -r line || [[ -n "$line" ]]; do
+    if echo $line | grep -F = &>/dev/null
+    then
+        DOMAIN=$(echo "$line" | cut -d '=' -f 1)
+        PUBDIR=/var/www/$(echo "$line" | cut -d '=' -f 2-)
 
-    echo "Updating vhost config for $DOMAIN..."
-    sudo sed -i s,scotchbox.local,$DOMAIN,g /etc/apache2/sites-available/$DOMAIN.conf
-    sudo sed -i s,/var/www/projects,/var/www/projects/bowboosh/dist,g /etc/apache2/sites-available/$DOMAIN.conf
+        echo "Creating vhost config for $DOMAIN..."
+        sudo cp /etc/apache2/sites-available/$DOMAIN_SAMPLE.conf /etc/apache2/sites-available/$DOMAIN.conf
 
-    echo "Enabling $DOMAIN. Will probably tell you to restart Apache..."
-    sudo a2ensite $DOMAIN.conf
+        echo "Updating public directory $PUBDIR for $DOMAIN..."
+        sudo sed -i s,/var/www/$DOMAIN_SAMPLE/public,$PUBDIR,g /etc/apache2/sites-available/$DOMAIN.conf
 
+        echo "Updating vhost alias and servername for $DOMAIN..."
+        sudo sed -i s,$DOMAIN_SAMPLE,$DOMAIN,g /etc/apache2/sites-available/$DOMAIN.conf
 
-# GROWER
-    DOMAIN=grower.local
-    echo "Creating vhost config for $DOMAIN..."
-    sudo cp /etc/apache2/sites-available/scotchbox.local.conf /etc/apache2/sites-available/$DOMAIN.conf
+        echo "Enabling $DOMAIN..."
+        sudo a2ensite $DOMAIN.conf
 
-    echo "Updating vhost config for $DOMAIN..."
-    sudo sed -i s,scotchbox.local,$DOMAIN,g /etc/apache2/sites-available/$DOMAIN.conf
-    sudo sed -i s,/var/www/projects,/var/www/projects/grower-js/public,g /etc/apache2/sites-available/$DOMAIN.conf
-
-    echo "Enabling $DOMAIN. Will probably tell you to restart Apache..."
-    sudo a2ensite $DOMAIN.conf
-
-
-# FAUBOURGSIMONE
-    DOMAIN=faubourgsimone.local
-    echo "Creating vhost config for $DOMAIN..."
-    sudo cp /etc/apache2/sites-available/scotchbox.local.conf /etc/apache2/sites-available/$DOMAIN.conf
-
-    echo "Updating vhost config for $DOMAIN..."
-    sudo sed -i s,scotchbox.local,$DOMAIN,g /etc/apache2/sites-available/$DOMAIN.conf
-    sudo sed -i s,/var/www/projects,/var/www/faubourgsimone/site,g /etc/apache2/sites-available/$DOMAIN.conf
-
-    echo "Enabling $DOMAIN. Will probably tell you to restart Apache..."
-    sudo a2ensite $DOMAIN.conf
+    fi
+done < ./$vhostFile
 
 # RESTART OR RELOAD APACHE
-    echo "So let's restart apache..."
-    #sudo service apache2 restart
-    sudo service apache2 reload
-
+    echo "Restarting Apache to enable default and scotchbox sites..."
+    #sudo service apache2 reload
+    sudo service apache2 restart
 
 # CREATING VARIABLE ENVIRONMENT
-    sudo bash -c \'echo export APP_ENV="development" >> /etc/apache2/envvars\'
-    sudo service apache2 restart
+#    sudo bash -c \'echo export APP_ENV="development" >> /etc/apache2/envvars\'
+#    sudo service apache2 restart
 
 # IMPORT DATABASE DUMP
     #mysql -u root -proot scotchbox < /var/www/dump.sql
