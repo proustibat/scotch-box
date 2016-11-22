39c740216b51ff184d305f8ac23f40d8d7c53ea4
Proustibat <jennifer.proust@gmail.com>
2016-11-23 00:38:25 +0100

    add synced folder for cordova apps and create specific vhosts list

--- Vagrantfile
+++ Vagrantfile
@@ -5,11 +5,14 @@
 # SERVER CONFIGURATION
 DOMAIN_SCOTCHBOX="scotchbox.local"
 DOMAIN_SAMPLE="sample.local"
-DOMAIN_DEFAULT="projects.local"
+DOMAIN_DEFAULT="sites.local"
+DOMAIN_APPS="apps.local"
 VAGRANTDIR="scotch-box-proustibat"
 vhostFile="vhosts.conf"
+vhostAppFile="apps-vhosts.conf"
 
 dirSynced="/workspace"
+dirSyncedApp="/space/www/apps"
 dbFbrgSmnSrc="/workspace/faubourgsimone/site/bdd/fs-web-local.sql"
 dbFbrgSmnFile="fs-web-local.sql"
 dbName="fs-web-local"
@@ -24,17 +27,36 @@ Vagrant.configure("2") do |config|
     config.vm.box = "scotch/box"
     config.vm.network "private_network", ip: "192.168.33.10"
     config.vm.hostname = "scotchbox"
-    config.vm.synced_folder dirSynced, "/var/www", :nfs => { :mount_options => ["dmode=777","fmode=666"] }
+    config.vm.synced_folder dirSynced, "/var/www/sites", id: "sites", :nfs => { :mount_options => ["dmode=777","fmode=666"] }
+    config.vm.synced_folder dirSyncedApp, "/var/www/apps", id: "apps", :nfs => { :mount_options => ["dmode=777","fmode=666"] }
+
+
+# Use NFS for the shared folder
+# config.vm.synced_folder dirSynced, "/var/www/sites",
+#       id: "sites",
+#       :nfs => true,
+#       :mount_options => ['nolock,vers=3,udp,noatime']
+
+# Use NFS for the shared folder
+# config.vm.synced_folder dirSyncedApp, "/var/www/apps",
+#       id: "apps",
+#       :nfs => true,
+#       :mount_options => ['nolock,vers=3,udp,noatime']
 
     config.vm.provision "file",
         source: vhostFile,
         destination: vhostFile,
         preserve_order: true
 
+    config.vm.provision "file",
+        source: vhostAppFile,
+        destination: vhostAppFile,
+        preserve_order: true
+
     config.vm.provision "shell",
         path: "provision-vhosts.sh",
         preserve_order: true,
-        args: [ DOMAIN_SCOTCHBOX, DOMAIN_SAMPLE, DOMAIN_DEFAULT, VAGRANTDIR, vhostFile ]
+        args: [ DOMAIN_SCOTCHBOX, DOMAIN_SAMPLE, DOMAIN_DEFAULT, VAGRANTDIR, vhostFile, vhostAppFile, DOMAIN_APPS ]
 
     config.vm.provision "file",
         source: dbFbrgSmnSrc,
--- provision-vhosts.sh
+++ provision-vhosts.sh
@@ -2,9 +2,11 @@
 
 if [[ -z $1 ]]; then DOMAIN_SCOTCHBOX="scotchbox.local";  else DOMAIN_SCOTCHBOX=$1; fi
 if [[ -z $2 ]]; then DOMAIN_SAMPLE="sample.local";        else DOMAIN_SAMPLE=$2; fi
-if [[ -z $3 ]]; then DOMAIN_DEFAULT="projects.local";     else DOMAIN_DEFAULT=$3; fi
+if [[ -z $3 ]]; then DOMAIN_DEFAULT="sites.local";        else DOMAIN_DEFAULT=$3; fi
 if [[ -z $4 ]]; then VAGRANTDIR="scotch-box-proustibat";  else VAGRANTDIR=$4; fi
 if [[ -z $5 ]]; then vhostFile="vhosts.conf";             else vhostFile=$5; fi
+if [[ -z $6 ]]; then vhostAppFile="apps-vhosts.conf";     else vhostAppFile=$6; fi
+if [[ -z $7 ]]; then DOMAIN_APPS="apps.local";            else DOMAIN_APPS=$7; fi
 
 
 # CREATE SAMPLE FILE
@@ -14,30 +16,57 @@ if [[ -z $5 ]]; then vhostFile="vhosts.conf";             else vhostFile=$5; fi
    sudo sed -i s,/var/www/public,/var/www/$DOMAIN_SAMPLE/public,g /etc/apache2/sites-available/$DOMAIN_SAMPLE.conf
    sudo sed -i s,/var/www/$VAGRANTDIR/public,/var/www/$DOMAIN_SAMPLE/public,g /etc/apache2/sites-available/$DOMAIN_SAMPLE.conf
 
-# DEFAULT HOST
+# DEFAULT HOST (SITES)
     echo "Setting $DOMAIN_DEFAULT as default host..."
     sudo cp /etc/apache2/sites-available/$DOMAIN_SAMPLE.conf /etc/apache2/sites-available/000-default.conf
     sudo sed -i s,$DOMAIN_SAMPLE,$DOMAIN_DEFAULT,g /etc/apache2/sites-available/000-default.conf
-    sudo sed -i s,/var/www/$DOMAIN_DEFAULT/public,/var/www/projects,g /etc/apache2/sites-available/000-default.conf
+    sudo sed -i s,/var/www/$DOMAIN_DEFAULT/public,/var/www/sites/projects,g /etc/apache2/sites-available/000-default.conf
 
 # SCOTCHBOX
-    echo "Updating vhost config for $DOMAIN_SCOTCHBOX..."
-    sudo sed -i s,/var/www/public,/var/www/$VAGRANTDIR/public,g /etc/apache2/sites-available/$DOMAIN_SCOTCHBOX.conf
-    sudo a2ensite $DOMAIN_SCOTCHBOX.conf
+#    echo "Updating vhost config for $DOMAIN_SCOTCHBOX..."
+#    sudo sed -i s,/var/www/public,/var/www/sites/$VAGRANTDIR/public,g /etc/apache2/sites-available/$DOMAIN_SCOTCHBOX.conf
+#    sudo a2ensite $DOMAIN_SCOTCHBOX.conf
+
+# APPS HOST
+    DOMAIN=$DOMAIN_APPS
+    PUBDIR=/var/www/apps
+    echo "Creating vhost config for $DOMAIN with $PUBDIR..."
+    sudo rm -f /etc/apache2/sites-available/$DOMAIN.conf
+    sudo cp /etc/apache2/sites-available/$DOMAIN_SAMPLE.conf /etc/apache2/sites-available/$DOMAIN.conf
+    sudo sed -i s,/var/www/$DOMAIN_SAMPLE/public,$PUBDIR,g /etc/apache2/sites-available/$DOMAIN.conf
+    sudo sed -i s,$DOMAIN_SAMPLE,$DOMAIN,g /etc/apache2/sites-available/$DOMAIN.conf
+    sudo a2ensite $DOMAIN.conf
 
 # CUSTOM VHOSTS
+echo "# CUSTOM VHOSTS"
 while IFS='' read -r line || [[ -n "$line" ]]; do
     if echo $line | grep -F = &>/dev/null
     then
         DOMAIN=$(echo "$line" | cut -d '=' -f 1)
-        PUBDIR=/var/www/$(echo "$line" | cut -d '=' -f 2-)
+        PUBDIR=/var/www/sites/$(echo "$line" | cut -d '=' -f 2-)
         echo "Creating vhost config for $DOMAIN with $PUBDIR..."
+        sudo rm -f /etc/apache2/sites-available/$DOMAIN.conf
         sudo cp /etc/apache2/sites-available/$DOMAIN_SAMPLE.conf /etc/apache2/sites-available/$DOMAIN.conf
         sudo sed -i s,/var/www/$DOMAIN_SAMPLE/public,$PUBDIR,g /etc/apache2/sites-available/$DOMAIN.conf
         sudo sed -i s,$DOMAIN_SAMPLE,$DOMAIN,g /etc/apache2/sites-available/$DOMAIN.conf
         sudo a2ensite $DOMAIN.conf
     fi
 done < ./$vhostFile
+
+# APPS VHOSTS
+echo "# APPS VHOSTS"
+while IFS='' read -r line || [[ -n "$line" ]]; do
+    if echo $line | grep -F = &>/dev/null
+    then
+        DOMAIN=$(echo "$line" | cut -d '=' -f 1)
+        PUBDIR=/var/www/apps/$(echo "$line" | cut -d '=' -f 2-)
+        echo "Creating vhost config for $DOMAIN with $PUBDIR..."
+        sudo cp /etc/apache2/sites-available/$DOMAIN_SAMPLE.conf /etc/apache2/sites-available/$DOMAIN.conf
+        sudo sed -i s,/var/www/$DOMAIN_SAMPLE/public,$PUBDIR,g /etc/apache2/sites-available/$DOMAIN.conf
+        sudo sed -i s,$DOMAIN_SAMPLE,$DOMAIN,g /etc/apache2/sites-available/$DOMAIN.conf
+        sudo a2ensite $DOMAIN.conf
+    fi
+done < ./$vhostAppFile
 
 # RESTART OR RELOAD APACHE
     sudo service apache2 restart
